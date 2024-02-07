330d80aa7ef09c4f2a86847cdfd8119e06eb6ba7
Kris10 <kmrichie611@gmail.com>
2024-02-06 19:05:39 -0600

    added react routes for navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import '../src/Components/createPostButton.css'
-import '../src/Components/loginButton.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+//import css
+import './index.css';
+import '../src/Components/createPostButton.css';
+import '../src/Components/loginButton.css';
+
+//import pages the router will use to conditionally show the appropriate views
+import App from './App.jsx';
+import Home from './pages/Hero';
+import About from './pages/About';
+import Donate from './pages/Donate';
+import ErrorPage from './pages/ErrorPage';
+
+const router = createBrowserRouter([
+  //defining accessible routes
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: '/about',
+        element: <About/>
+      },
+      {
+        path: './donate',
+        element: <Donate />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
+
+
