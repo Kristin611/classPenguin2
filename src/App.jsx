06a733fcf2e4df52ce8b0d003343d715340a22ad
Nikki Vargas <angela94.chavez@gmail.com>
2024-02-06 20:58:47 -0600

    updated stuff twice

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import LoginButton from './Components/loginButton';
-import CreatePostButton from './Components/createPost';
 
+import './App.css'
+import Navbar from './components/Navbar';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <LoginButton /> 
-      <CreatePostButton />
+      <Navbar />
     </>
   )
 }
