7c4ff895c23b5439d89279d8784350c2ed001793
Kris10 <kmrichie611@gmail.com>
2024-02-14 12:20:15 -0600

    env file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGOODB_URI ||'mongodb://127.0.0.1:27017/Blog')
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://127.0.0.1:27017/Blog')
 
 
 
